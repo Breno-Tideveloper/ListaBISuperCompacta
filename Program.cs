b48fd2fb9f34062fac1cfa95e938a1c2e026dd37
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 10:27:01 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ListaBISuperCompacta.BibliotecaListaBI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaBISuperCompacta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listaBI = new ListaBI();
+            Console.WriteLine("digite o nome que deseja pesquisar");
+
+            listaBI.PesquisaLista(Console.ReadLine());
+
+            Console.ReadKey();
+        }
+    }
+}
